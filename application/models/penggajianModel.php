ab31677d137fb42dc93ea6780eb55950633e6292
tesais <akuecaa19@gmail.com>
2024-12-25 08:57:39 +0700

    Menambahkan file penggajian

---
+++ application/models/penggajianModel.php
@@ -0,0 +1,17 @@
+<?php 
+
+class PenggajianModel extends CI_model{
+    public function get_data($table){
+        return $this->db->get($table);
+    }
+
+    public function insert_data($data,$table){
+        $this->db->insert($table,$data);
+    }
+
+    public function update_data($table, $data, $where){
+        $this->db->update($table, $data, $where);
+    }
+}
+
+?>
